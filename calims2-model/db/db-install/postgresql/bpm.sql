80ff9900ed01d4e8bfc5c5e9149fc0bf7241bf04
Mahidhar Narra <narram@mail.nih.gov>
2013-04-22 11:17:57 -0400

    adding license

--- calims2-model/db/db-install/postgresql/bpm.sql
+++
@@ -1,8 +0,0 @@
-/*L
-  Copyright Moxie Informatics.
-
-  Distributed under the OSI-approved BSD 3-Clause License.
-  See http://ncip.github.com/calims/LICENSE.txt for details.
-L*/
-
-insert into task_organizationalentity (id,dtype) values ('Administrator','User');
--- calims2-webapp/src/dojo/profiles/dev.js
+++
@@ -1,8 +0,0 @@
-dependencies = {
-  
-  prefixes: [
-    [ "dijit", "../dijit" ],
-    [ "dojox", "../dojox" ],
-    [ "calims2", "../../modules/calims2" ]
-  ]
-}
